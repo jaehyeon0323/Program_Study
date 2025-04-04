2c06f2ef259220a11c5684cc7627207214f88398
명재현 <moung9486@gmail.com>
2025-04-04 15:26:24 +0900

    feat: 305page TodoList.js 생성완료

---
+++ FrontEnd/todolist/src/component/TodoEditor.js
@@ -0,0 +1,13 @@
+import './TodoEditor.css';
+
+export const TodoEditor = () => {
+  return(
+    <div className="TodoEditor">
+      <h4>새로운 Todo 작성하기 ✏️</h4>
+      <div className = "editor_wrapper">
+        <input placeholder = "새로운 Todo ..."/>
+        <button>추가</button>
+      </div>
+    </div>
+  );
+}
---
+++ FrontEnd/todolist/src/component/TodoList.js
@@ -0,0 +1,10 @@
+import './TodoList.css'
+
+export const TodoList = () =>{
+  return(
+    <div className = "TodoList">
+      <h4>Todo List 🌿</h4>
+      <input className = "searchbar" placeholder = "검색어를 입력하세요"/>
+    </div>
+  );
+}
